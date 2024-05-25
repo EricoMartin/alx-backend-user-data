75267f30c15604d77cc0207605ca277a77ce9526
EricoMartin <ericomartin.0785@gmail.com>
2024-05-25 01:51:07 +0000

    Auth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Auth Class
+"""
+
+from flask import request
+from typing import (
+    List,
+    TypeVar
+)
+
+
+class Auth:
+    """
+    API authentication
+    """
+
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+            """
+            Determines whether a given path requires authentication or not
+            Args:
+                - path(str): Url path to be checked
+                - excluded_paths(List of str): List of paths that do not require
+                authentication
+            Return:
+                - True if path is not in excluded_paths, else False
+            """
+            return False
+
+
+    def authorization_header(self, request=None) -> str:
+            """
+            returns authorization header info from Flask request object
+            """
+            return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+            """
+            Returns a User instance from information from a request object
+            """
+            return None
