1496fa87b7862d8214b23fb6ef207d46159480f9
Eric Ibu <ericomartin.0785@gmail.com>
2024-05-25 03:19:57 +0100

    auth header

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -46,7 +46,12 @@ class Auth:
         """
         returns authorization header info from Flask request object
         """
-        return None
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+        if header is None:
+            return None
+        return header
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
